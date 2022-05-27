71be810bf7c04c4de2a786ff74a10db5fcd20594
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2022-05-28 03:05:39 +0700

    inital project, config datasouce, jpa

---
+++ src/main/java/com/ctf/backendcollegeregistrationsystem/BackendCollegeRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ctf.backendcollegeregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCollegeRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCollegeRegistrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ctf/backendcollegeregistrationsystem/BackendCollegeRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctf.backendcollegeregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCollegeRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
